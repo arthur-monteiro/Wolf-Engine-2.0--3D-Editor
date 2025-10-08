ded82b546472bc1ea3c28c7d5b8067a7c06299f5
Arthur MONTEIRO <atou.monteiro@gmail.com>
2025-10-08 15:23:00 +0200

    update cmake files to only use them instead of visual studio files

---
+++ set_wolf_engine_symlink.bat
@@ -0,0 +1,2 @@
+mkdir Dependencies
+mklink /d "./Dependencies/wolfenginecontent-src" "%WOLF_ENGINE_PATH%"
--- setup_wolf_engine.bat
+++
@@ -1,2 +0,0 @@
-mkdir Dependencies
-mklink /d "./Dependencies/Wolf-Engine-2.0" "%WOLF_ENGINE_PATH%"
